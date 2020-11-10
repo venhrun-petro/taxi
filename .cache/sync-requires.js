04ed0f9afdbde0a6b09d223de0104e3e94734767
p.venhryn <p.venhryn@pozitiff.ua>
2020-11-10 15:27:53 +0200

    update_site

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,6 +5,7 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/pvenhryn/apollo/taxi/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/home/pvenhryn/apollo/taxi/src/pages/404.js"))),
   "component---src-templates-home-js": hot(preferDefault(require("/home/pvenhryn/apollo/taxi/src/templates/home.js")))
 }
